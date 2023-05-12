bc8243aa1080cc2a2b3527f7ed94af3dc1186308
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-05-12 21:14:45 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="bg-green-600">mamad</p>
+    </>
+  );
+}
+
+export default App;
